22639418a799fff3a02791002fad25cd7b7c7fc1
Tanujshahh <tanujtushar.shah@my.jcu.edu.au>
2017-09-30 11:51:45 +0800

    Add files via upload

---
+++ SuperTrump.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Tanuj Tushar Shah 13409858
+ * git https://github.com/Tanujshahh/CP2406Assignment-2
+ */
+public class SuperTrump {
+    public static void main(String[] args) {
+        startGame();
+    }
+
+    private static void startGame() {
+        new TrumpGame();
+
+    }
+}
